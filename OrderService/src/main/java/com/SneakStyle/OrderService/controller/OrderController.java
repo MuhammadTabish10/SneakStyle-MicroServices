5d13d11d5507a1826d6fe22271165beae66cb130
Muhammad Tabish <muhammadtabish05@gmail.com>
2024-05-02 16:17:00 +0500

    Circuit Breaker completed

--- OrderService/src/main/java/com/SneakStyle/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/com/SneakStyle/OrderService/controller/OrderController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,36 +24,96 @@ public class OrderController {
 
     private OrderService orderService;
     @PostMapping("/order")
-//    @CircuitBreaker(name = "addOrderBreaker", fallbackMethod = "addOrderFallBack")
+    @CircuitBreaker(name = "addOrderBreaker", fallbackMethod = "addOrderFallBack")
     public ResponseEntity<OrderDto> addOrder(@Valid @RequestBody OrderDto orderDto) {
         OrderDto order = orderService.createOrder(orderDto);
         return ResponseEntity.ok(order);
     }
     public ResponseEntity<OrderDto> addOrderFallBack(OrderDto orderDto, Exception ex){
         log.info("Fallback is executed because service is down: ", ex.getMessage());
-        return new ResponseEntity<>(orderDto, HttpStatus.OK);
+        OrderDto errorOrderDto = OrderDto.builder()
+                .orderStatus(OrderStatus.ERROR)
+                .totalAmount(0.0)
+                .date(LocalDate.now())
+                .status(false)
+                .build();
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(errorOrderDto);
     }
     @GetMapping("/order")
+    @CircuitBreaker(name = "getAllOrdersByOrderStatusBreaker", fallbackMethod = "getAllOrdersByOrderStatusFallBack")
     public ResponseEntity<List<OrderDto>> getAllOrdersByOrderStatus(@RequestParam(name = "status") String status) {
         List<OrderDto> orderDtoList = orderService.getAllByOrderStatus(status);
         return ResponseEntity.ok(orderDtoList);
     }
+    public ResponseEntity<List<OrderDto>> getAllOrdersByOrderStatusFallBack(String status, Exception ex){
+        log.info("Fallback is executed because service is down: ", ex.getMessage());
+        List<OrderDto> orders = new ArrayList<>();
+        OrderDto errorOrderDto = OrderDto.builder()
+                .orderStatus(OrderStatus.ERROR)
+                .totalAmount(0.0)
+                .date(LocalDate.now())
+                .status(false)
+                .build();
+        orders.add(errorOrderDto);
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(orders);
+    }
+
     @GetMapping("/order/status/{status}")
+    @CircuitBreaker(name = "getAllOrdersBreaker", fallbackMethod = "getAllOrdersFallBack")
     public ResponseEntity<List<OrderDto>> getAllOrders(@PathVariable Boolean status) {
         List<OrderDto> orderDtoList = orderService.getAllOrders(status);
         return ResponseEntity.ok(orderDtoList);
     }
+    public ResponseEntity<List<OrderDto>> getAllOrdersFallBack(Boolean status, Exception ex){
+        log.info("Fallback is executed because service is down: ", ex.getMessage());
+        List<OrderDto> orders = new ArrayList<>();
+        OrderDto errorOrderDto = OrderDto.builder()
+                .orderStatus(OrderStatus.ERROR)
+                .totalAmount(0.0)
+                .date(LocalDate.now())
+                .status(false)
+                .build();
+        orders.add(errorOrderDto);
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(orders);
+    }
+
     @GetMapping("/order/date/{date}")
+    @CircuitBreaker(name = "getAllOrdersByDateBreaker", fallbackMethod = "getAllOrdersByDateFallBack")
     public ResponseEntity<List<OrderDto>> getAllOrdersByDate(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         List<OrderDto> orderDtoList = orderService.getAllOrdersByDate(date);
         return ResponseEntity.ok(orderDtoList);
     }
+    public ResponseEntity<List<OrderDto>> getAllOrdersByDateFallBack(LocalDate date, Exception ex){
+        log.info("Fallback is executed because service is down: ", ex.getMessage());
+        List<OrderDto> orders = new ArrayList<>();
+        OrderDto errorOrderDto = OrderDto.builder()
+                .orderStatus(OrderStatus.ERROR)
+                .totalAmount(0.0)
+                .date(LocalDate.now())
+                .status(false)
+                .build();
+        orders.add(errorOrderDto);
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(orders);
+    }
     @GetMapping("/order/dates")
+    @CircuitBreaker(name = "getAllOrdersBetweenDatesBreaker", fallbackMethod = "getAllOrdersBetweenDatesFallBack")
     public ResponseEntity<List<OrderDto>> getAllOrdersBetweenDates(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
         List<OrderDto> orderDtoList = orderService.getAllOrdersBetweenDates(startDate, endDate);
         return ResponseEntity.ok(orderDtoList);
+    }
+    public ResponseEntity<List<OrderDto>> getAllOrdersBetweenDatesFallBack(LocalDate startDate, LocalDate endDate, Exception ex){
+        log.info("Fallback is executed because service is down: ", ex.getMessage());
+        List<OrderDto> orders = new ArrayList<>();
+        OrderDto errorOrderDto = OrderDto.builder()
+                .orderStatus(OrderStatus.ERROR)
+                .totalAmount(0.0)
+                .date(LocalDate.now())
+                .status(false)
+                .build();
+        orders.add(errorOrderDto);
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(orders);
     }
     @GetMapping("/order/{id}")
     @CircuitBreaker(name = "getOrderByIdBreaker", fallbackMethod = "getOrderByIdFallBack")
@@ -60,7 +121,7 @@ public class OrderController {
         OrderDto order = orderService.getOrderById(id);
         return ResponseEntity.ok(order);
     }
-    public ResponseEntity<OrderDto> getOrderByIdFallBack(Long id, Throwable ex){
+    public ResponseEntity<OrderDto> getOrderByIdFallBack(Long id, Exception ex){
         log.info("Fallback is executed because service is down: ", ex.getMessage());
         OrderDto errorOrderDto = OrderDto.builder()
                 .id(id)
